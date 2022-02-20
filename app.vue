6db8ec626618028bbeff2a4f303a6992c4b11400
dethMastery <owner@detzz.in.th>
2022-02-20 21:53:53 +0700

    added: setting up page

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div id="app">
+        hello
+    </div>
 </template>
+
+<style>
+@import url(./styles/style.css);
+</style>
+
+<script setup>
+useMeta({
+  title: 'Suphakit P. | detzz.in.th',
+  meta: [
+      {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, maximum-scale=1',
+      }
+  ]
+});
+</script>

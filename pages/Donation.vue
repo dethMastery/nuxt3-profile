9bd92396f9fe86ea0ce3e5df7d82c65aacb698fa
dethMastery <owner@detzz.in.th>
2022-03-07 09:48:55 +0700

    added: setting up donation pagr

---
+++ pages/Donation.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="donation" class="contain">
+
+    </div>
+</template>
+
+<style>
+@import url(../styles/style.css);
+@import url(../styles/animate.css);
+@import url(../styles/mobile.css);
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@300&display=swap');
+</style>
+
+<script setup>
+    useMeta({
+    title: 'Donation | detzz.in.th',
+    meta: [
+        {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1, maximum-scale=1',
+        }
+    ]
+    });
+
+    let api = "https://api.detzz.in.th/profile";
+    let {data: resp} = await useFetch(api);
+</script>

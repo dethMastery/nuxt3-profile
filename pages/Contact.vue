804d5ed754a08758529b8f1a50ab6431b0542472
dethMastery <owner@detzz.in.th>
2022-03-11 17:44:28 +0700

    added: contact page

---
+++ pages/Contact.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+        <div id="donation" class="contain">
+            <div class="overlay"></div>
+        </div>
+        
+        <div class="c-c-cont">
+            <div class="img-header">
+                <img src="/prof.webp" alt="profile">
+            </div>
+            <div class="text-content-card">
+                <h2>
+                    <u>
+                        Contact
+                    </u>
+                </h2>
+                <!-- <p v-for="donate in resp[0].donation" :key="donate.id">
+                    {{donate.name}} : {{donate.target}}
+                </p>
+                <br>
+                <span>
+                    {{resp[0].donation[0].uName}}
+                </span> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+@import url(../styles/style.css);
+@import url(../styles/donation.css);
+@import url(../styles/animate.css);
+@import url(../styles/mobile.css);
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@300&display=swap');
+</style>
+
+<script setup>
+    useMeta({
+    title: 'Donation | detzz.in.th',
+    meta: [
+        {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1, maximum-scale=1',
+        }
+    ]
+    });
+
+    let api = "https://api.detzz.in.th/profile";
+    let {data: resp} = await useFetch(api);
+</script>
